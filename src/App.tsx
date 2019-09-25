fbf87832b5876fac8934606f7365961acd430977
Marten Veldthuis <marten@veldthuis.com>
2019-09-25 20:09:21 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3';
+import { number } from 'prop-types';
+
+class Automation {
+  private schedules: Schedule[];
+  
+  constructor() {
+    this.schedules = [];
+  }
+
+  addSchedule(schedule: Schedule) {
+    this.schedules.push(schedule);
+  }
+}
+
+type ID = number;
+
+interface DayEvent {
+  delay: number;
+  measurementId: ID;
+}
+
+interface DailySchedule {
+  id: ID;
+  scheduler: string;
+  dayEvents: DayEvent[];
+}
+
+interface IntervalSchedule {
+  id: ID;
+  scheduler: string;
+}
+
+type Schedule = DailySchedule | IntervalSchedule;
+
+function demoAutomation() {
+  const automation = new Automation();
+
+  const dailySchedule = {id: 1, scheduler: "daily", dayEvents: [
+    {delay: 0, measurementId: 1},
+    {delay: 14400, measurementId: 1}
+  ]};
+
+  automation.addSchedule(dailySchedule);
+
+  return automation;
+}
+
+
+
+
+interface EditorProps {
+  automation: Automation;
+}
+
+const Editor: React.FC<EditorProps> = ({ automation }) => {
+  return <div>foo</div>;
+}
+
+interface PreviewProps {
+  automation: Automation;
+}
+
+class Preview extends React.Component<PreviewProps, {}> {
+  private svgRef: React.RefObject<SVGSVGElement>;
+
+  constructor(props: PreviewProps) {
+    super(props);
+    this.svgRef = React.createRef();
+  }
+
+  componentDidMount() {
+    d3.select(this.svgRef.current);
+  }
+
+  render() {
+    return <svg ref={this.svgRef}></svg>
+  }
+}
+
 
 const App: React.FC = () => {
+  const [automation, setAutomation] = React.useState(demoAutomation());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Editor</header>
+      <section>
+        <Editor automation={automation} />
+      </section>
+
+      <header>EPD</header>
+      <section>
+        <Preview automation={automation} />
+      </section>
     </div>
   );
 }
